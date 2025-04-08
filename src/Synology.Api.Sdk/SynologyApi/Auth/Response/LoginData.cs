568a5d6581f6cd4bcf0c486c00450a3fb4063049
Esau Silva <esausilva85@gmail.com>
2025-04-08 11:20:08 -0500

    Add new properties to LoginData class
    Added `Account`, `DeviceId`, and `IkMessage` fields to the `LoginData` class. These properties enhance the data model by providing additional authentication-related information. Adjusted JSON property mappings where needed to ensure proper serialization.

--- src/Synology.Api.Sdk/SynologyApi/Auth/Response/LoginData.cs
+++ src/Synology.Api.Sdk/SynologyApi/Auth/Response/LoginData.cs
@@ -4,9 +4,17 @@ namespace Synology.Api.Sdk.SynologyApi.Auth.Response;
 
 public sealed class LoginData
 {
-    public string Did { get; init; } = string.Empty;
     public string Sid { get; init; } = string.Empty;
+
+    public string Account { get; init; } = string.Empty;
+
     public string? SynoToken { get; init; }
+
+    [JsonPropertyName("device_id")]
+    public string DeviceId { get; init; } = string.Empty;
+
+    [JsonPropertyName("ik_message")]
+    public string IkMessage { get; init; } = string.Empty;
 
     [JsonPropertyName("is_portal_port")]
     public bool IsPortalPort { get; init; }
